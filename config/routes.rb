0a338dcec4218ee0ffb943f4d1ecd7bdf741d1b3
Mateusz Staszczyk <mateusz.staszczyk@netguru.co>
2019-10-10 19:18:16 +0200

    Add pages controller and root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   use_doorkeeper
   mount API::Base, at: '/'
   mount GrapeSwaggerRails::Engine => '/swagger'
+
+  root 'pages#home'
+  match '*path', to: 'pages#home', via: :all
 end
